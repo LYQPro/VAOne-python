5b177302a0fe05374a05fd006308afd194d218f4
lyq10085 <52818035+lyq10085@users.noreply.github.com>
2022-08-08 15:49:42 +0800

    Update __init__.py

--- vvisualization/__init__.py
+++ vvisualization/__init__.py
@@ -3,3 +3,4 @@ from vvisualization.tools import glv_config as glvc
 from vvisualization.read.trimlayer import Fiber, Foam
 from vvisualization.mix import layerup
 from vvisualization.apply.apply import *
+//useless update
